22b071e85d63a3cd274bf6849177780a0831697c
kerptwo <egorov@itexp.pro>
2025-02-09 21:53:32 +0100

    sprint-2

--- src/components/common/ChatWindoow/ChatWindoow.ts
+++ src/components/common/ChatWindoow/ChatWindoow.ts
@@ -8,22 +8,18 @@ export default class ChatWindow extends Block {
   }
 
   afterRender(): void {
-    // Находим форму отправки сообщения по id "send-message"
     const form = this.getContent().querySelector("#send-message");
     if (form) {
-      // Находим контейнер для поля ввода сообщения внутри формы
       const messageInputContainer = form.querySelector(
         "#message-input-container"
       );
       if (messageInputContainer) {
         messageInputContainer.innerHTML = "";
-        // Создаем экземпляр компонента Input для сообщения
         const messageInput = new Input({
           id: "message",
           type: "text",
           placeholder: "Сообщение",
         });
-        // Рендерим компонент и вставляем его в контейнер
         messageInputContainer.appendChild(messageInput.render());
       }
     }
